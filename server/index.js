acaabba3b86eef81a0d1d05c12298e429bf283db
Harsh Kumar Pandey <121681237+harsh1kumar1@users.noreply.github.com>
2023-06-12 10:56:24 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -13,14 +13,13 @@ dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors({
-   origin: ["https://friendsmemories.netlify.app/","http://localhost:3000"],
-}));
+app.use(cors());
 
 app.use('/posts',postRoutes)
 app.use('/user',userRoutes)
 
 app.use('/',(req,res)=>{
+   response.setHeader("Access-Control-Allow-Credentials", "true");
     res.send("hello to Memories API");    
 })
 
